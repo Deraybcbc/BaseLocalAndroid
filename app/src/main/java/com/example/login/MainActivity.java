f5e4109ed793063afa4f9bb7aa8249dad5210bcf
Deraybcbc <a22kevburcac@inspedralbes.cat>
2023-10-17 12:27:03 +0200

    Echo el Intent, falta hacer el menu

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button boto = (Button) findViewById(R.id.Iniciar);
+
+        boto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        EditText user = (EditText) findViewById(R.id.User);
+
+        String users = user.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        intent.putExtra("Usuario",users);
+
+        startActivity(intent);
+    }
+}
